498862f8243272123bec5f62c21787ae2ba61d31
Glerison Lima <glerisonlima@gmail.com>
2024-05-16 00:13:40 -0300

    criando endpoint de ocorrencia

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ app.get("/", (req,res) => {
     res.send("Hello World!")
 })
 
+app.post("/ocorrencia", (req,res)=> {
+    console.log(req)
+})
+
 app.listen(PORT, ()=>{
     console.log("Servidor rodando!")
 })
