e755b93600b7d6b96e8538ee4b47783fcbb075e2
Glerison Lima <glerisonlima@gmail.com>
2024-05-16 09:29:58 -0300

    ajuste endpoint ocorrencia

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import 'dotenv/config'
 
-const app = express()
 const {PORT} = process.env
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/", (req,res) => {
     res.send("Hello World!")
@@ -10,6 +16,8 @@ app.get("/", (req,res) => {
 
 app.post("/ocorrencia", (req,res)=> {
     console.log(req.body)
+
+    return res.status(200).send('Status ok')
 })
 
 app.listen(PORT, ()=>{
